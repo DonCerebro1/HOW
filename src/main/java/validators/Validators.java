63cf790f0dc4641e292b0cfceb20317e30eafda6
Takeshi Nakamoto <85444482+DonCerebro1@users.noreply.github.com>
2022-07-01 21:16:48 +0200

    validators um Input in Textfeldern einzugrenzen - HOW-8

--- src/main/java/validators/Validators.java
+++ src/main/java/validators/Validators.java
@@ -1,2 +1,17 @@
-package validators;public class Validators {
+package validators;
+
+import javafx.scene.control.TextField;
+
+import java.util.regex.Pattern;
+
+public interface Validators {
+    Pattern letters = Pattern.compile("[a-zA-Z]");
+    Pattern number = Pattern.compile("[0-9]");
+
+    static boolean checkName(String str){
+        return letters.matcher(str).find();
+    }
+    static boolean checkPhoneNumber(String str){
+        return number.matcher(str).find();
+    }
 }
